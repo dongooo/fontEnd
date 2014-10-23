2409025dd1dad045780c9ab9c5e3db01833c8699
cookmen <854936376@qq.com>
2014-10-23 13:48:40 +0800

    Initial commit

---
+++ app/scripts/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name fontEndApp
+ * @description
+ * # fontEndApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('fontEndApp', []);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,18 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name fontEndApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the fontEndApp
+ */
+angular.module('fontEndApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma',
+	  'wdf'
+    ];
+  });
